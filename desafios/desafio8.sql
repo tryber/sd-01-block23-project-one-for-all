3ec16c9e8c320a5f3a68c7b9d39729a0943a257d
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-11 16:18:33 -0300

    desafio 8 arrumando

--- desafios/desafio8.sql
+++ desafios/desafio8.sql
@@ -5,11 +5,10 @@ DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
+BEGIN
 DELETE FROM artistas_seguidos ar WHERE ar.usuario_id = OLD.usuario_id;
 DELETE FROM historico_reproducao hr WHERE hr.usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
-
 DELETE FROM usuarios WHERE usuario_id = 15;
-SELECT * FROM artistas_seguidos;
