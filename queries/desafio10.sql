2de699c5bfec8a72f60f59c373bde91a4fa71e06
vrramosm <vf.ramos7@gmail.com>
2020-03-11 17:17:48 -0300

    Consertado erro de identaçao no desafio 10

--- queries/desafio10.sql
+++ queries/desafio10.sql
@@ -6,10 +6,10 @@ CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_de_musicas INT;
-  SELECT COUNT(musica_id)  
+SELECT COUNT(musica_id)  
 FROM historico_reproducao
 WHERE usuario_id = id INTO total_de_musicas;
-  RETURN total_de_musicas;
+RETURN total_de_musicas;
 END
 
 $$ DELIMITER ;
