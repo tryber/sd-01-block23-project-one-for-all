815d21c05393fcd65450bd53c75be4a2c2bf8c09
henriqueeyer <henriqueeyer@outlook.com>
2020-03-11 18:40:14 -0300

    1ao7

---
+++ desafio8.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON usuario
+    FOR EACH ROW
+BEGIN
+    DELETE FROM historico_reproducao
+    where historico_reproducao.usuario_id = OLD.usuario_id;
+    DELETE FROM seguindo
+    where seguindo.usuario_id = OLD.usuario_id;
+END; $$
+DELIMITER ;
+
+DELETE FROM usuario
+    where usuario_id = 15;
