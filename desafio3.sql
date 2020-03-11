b167e4c27ba1fcc29705f20e8c448c73915c5c24
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-03-11 19:21:37 -0300

    desafio 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,9 @@
 USE SpotifyClone;
-SHOW TABLES;
 
+CREATE VIEW historico_reproducao_usuarios AS
 SELECT ( SELECT name FROM users WHERE id = ph.user_id ) AS usuario,
 ( SELECT title FROM songs WHERE id = ph.song_id) AS nome
 FROM playback_history AS ph
 ORDER BY usuario ASC, nome ASC;
+
+SELECT * FROM historico_reproducao_usuarios;
